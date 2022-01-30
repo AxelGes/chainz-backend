99d7ef503f1c016527db3baa4b09f0ea7656d61f
AxelGes <axelges9@gmail.com>
2022-01-30 01:04:21 -0300

    restrict wallet uniqueness

--- src/entities/Player.ts
+++ src/entities/Player.ts
@@ -1,33 +1,41 @@
-import {Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn, OneToOne, BaseEntity, JoinColumn} from "typeorm";
-import { Skywars } from "./Skywars";
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  OneToOne,
+  BaseEntity,
+  JoinColumn
+} from 'typeorm';
+import { Skywars } from './Skywars';
 @Entity()
 export class Player extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @PrimaryGeneratedColumn()
-    id: number;
+  @Column({ unique: true })
+  uuid: string;
 
-    @Column({unique: true})
-    uuid: string;
+  @Column()
+  username: string;
 
-    @Column()
-    username: string;
+  @Column({ nullable: false, unique: true })
+  wallet: string;
 
-    @Column({ nullable: false })
-    wallet: string;
+  @Column({ default: 0 })
+  coins: number;
 
-    @Column({ default: 0 })
-    coins: number;
+  @Column({ nullable: false })
+  last_connection: Date;
 
-    @Column({ nullable: false })
-    last_connection: Date;
+  @OneToOne(() => Skywars, skywars => skywars.player, { cascade: true })
+  @JoinColumn()
+  skywars: Skywars;
 
-    @OneToOne(() => Skywars, skywars => skywars.player, { cascade: true })
-    @JoinColumn()
-    skywars: Skywars;
+  @CreateDateColumn()
+  created_at: Date;
 
-    @CreateDateColumn()
-    created_at: Date;
-    
-    @UpdateDateColumn()
-    updated_at: Date;
+  @UpdateDateColumn()
+  updated_at: Date;
 }
