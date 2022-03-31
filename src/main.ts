c3e28f8afaaae3cf8d4249aae356703cc47505b8
Esteban Viera <esteban.viera.arroba@gmail.com>
2022-03-31 00:57:55 -0300

    Added SSL support

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,34 @@
 import { NestFactory } from '@nestjs/core';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express';
+import * as fs from 'fs';
+import * as http from 'http';
+import * as https from 'https';
+
 import { AppModule } from './app.module';
 
+const crPath = '/etc/letsencrypt/live/mechabrawlers.com/fullchain.pem';
+const pkPath = '/etc/letsencrypt/live/mechabrawlers.com/privkey.pem';
+
+const options: any = {};
+
+if (fs.existsSync(crPath) && fs.existsSync(pkPath)) {
+  options.httpsOptions = {
+    cert: fs.readFileSync(crPath),
+    key: fs.readFileSync(pkPath)
+  }
+}
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const server = express();
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server), { cors: true });
+
+  await app.init();
+
+  http.createServer(server).listen(3000);
+  https.createServer(options, server).listen(443);
+
   await app.listen(8080);
 }
+
 bootstrap();
